11af4bce31b2c4de05d519e07e0828abf936e0e6
Jon Ander <jonanderdecastro@gmail.com>
2020-11-11 13:23:23 +0100

    imagenes y nuevo paso

--- miJS/wikiJ.js
+++ miJS/wikiJ.js
@@ -95,18 +95,20 @@ var arrayFotosProxmoxVM=new Array(
     "img/proxmoxVM/paso5.png",
     "img/proxmoxVM/paso6.png",
     "img/proxmoxVM/paso7.png",
-    "img/proxmoxVM/paso8.png"
+    "img/proxmoxVM/paso8.png",
+    "img/proxmoxVM/paso9.png"
 );
 
 var arrayContenidoProxmoxVM=new Array(
-    "Para realizar las copias de ubuntu, instalaremos FreeNAS  dentro de proxmox. Si no tenemos ninguna iso en el servidor subiremos una desde el ordenador.",
+    "Para crear una máquina necesitaremos una imagen iso. Si no tenemos ninguna iso en el servidor subiremos una desde el ordenador.",
     "Seleccionamos una iso desde nuestro ordenador.",
     "Seleccionaremos el botón situado en el lado superior derecho.",
     "Nos aparecerá la siguiente ventana. Le damos un nombre e ID.",
     "Seleccionamos la iso del sistema.",
     "Le damos el espacio deseado al disco.",
     "Le damos la cantidad de núcleos.",
-    "La cantidad de memoria ram."
+    "La cantidad de memoria ram.",
+    "Una vez se hayan configurado todos los apartados, nos mostrará un resumen. Podremos seleccionar que se ejecute inmediatamente después  de pulsar el botón Finish."
 );
 
 var imgAnterior=document.getElementById("anterior").addEventListener("click",anteriorFotoTutorialB);
